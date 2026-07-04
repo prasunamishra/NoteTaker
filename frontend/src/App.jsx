7d60f4c3865f2f304e8cb846144f843ab47ad42a
prasunamishra <np03cs4s250105@heraldcollege.edu.np>
2026-07-04 13:23:23 +0545

    Added search Bar and adjusted the css of the app using tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,29 @@
 import NoteGrid from "./components/NoteGrid"
-import NewNoteButton from "./components/NewNoteButton";
+import NewNoteButton from "./components/NewNoteButton"
+import SearchBar from "./components/SearchBar"
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold text-center mt-10">
+
+    <div className="min-h-screen bg-gray-100 p-6">
+
+      <h1 className="text-4xl font-bold text-center mb-8">
         Note Taker App
       </h1>
-    <div>
-       <div> <NewNoteButton /></div>
-       
-      <NoteGrid />
+
+      <div className="flex flex-col md:flex-row justify-between items-center gap-4 mb-8">
+
+        <SearchBar />
+
+        <NewNoteButton />
+
       </div>
-    </>
+
+      <NoteGrid />
+
+    </div>
+
   )
 }
 
