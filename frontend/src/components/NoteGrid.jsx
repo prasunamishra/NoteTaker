fbb548a2e45f23afd9532fadc2e61a86c270bfec
prasunamishra <np03cs4s250105@heraldcollege.edu.np>
2026-07-06 09:51:21 +0545

    Used props so that the NoteGrid receives the notes using that

--- frontend/src/components/NoteGrid.jsx
+++ frontend/src/components/NoteGrid.jsx
@@ -1,44 +1,16 @@
 import NoteCard from "./NoteCard"
 
-function NoteGrid() {
-
-  const notes = [
-
-    {
-      title: "Buy Groceries",
-      content: "Milk, Bread, Eggs",
-      category: "Personal"
-    },
-
-    {
-      title: "Project Meeting",
-      content: "Discuss frontend design",
-      category: "Work"
-    },
-
-    {
-      title: "Study React",
-      content: "Learn components and props",
-      category: "Study"
-    },
-
-    {
-      title: "Morning Exercise",
-      content: "30 minutes jogging",
-      category: "Personal"
-    }
-
-  ]
+function NoteGrid({ notes }) {
 
   return (
 
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-6">
 
       {
-        notes.map((note, index) => (
+        notes.map((note) => (
 
           <NoteCard
-            key={index}
+            key={note.id}
             title={note.title}
             content={note.content}
             category={note.category}
