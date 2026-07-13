976e00f8482c87546a2e6e08cb51910eb7d3f906
prasunamishra <np03cs4s250105@heraldcollege.edu.np>
2026-07-13 12:15:09 +0545

    MongoDB connected Succesfully

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 import app from './src/app.js';
+import connectDB from './src/config/db.js';
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Backend is running on port ${PORT}`);
-});
+const start = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Backend is running on port ${PORT}`);
+  });
+};
+
+start();
